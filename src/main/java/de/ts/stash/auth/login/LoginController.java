0f39dc29ec01dca83aa29bd68ae16d2cac56ea62
MuesLee <stimo@gmx.net>
2019-02-18 08:51:00 +0100

    Merge

--- src/main/java/de/ts/stash/auth/login/LoginController.java
+++ src/main/java/de/ts/stash/auth/login/LoginController.java
@@ -1,4 +1,4 @@
-package de.ts.stash;
+package de.ts.stash.auth.login;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
