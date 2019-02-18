662158768550b007805ecb53e393c4c27a421ade
MuesLee <stimo@gmx.net>
2019-02-18 08:48:30 +0100

    Initial commit

---
+++ src/main/java/de/ts/stash/LoginController.java
@@ -0,0 +1,13 @@
+package de.ts.stash;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+	 @RequestMapping("/")
+	    public String index() {
+	        return "Greetings from Spring Boot!";
+	    }
+}
