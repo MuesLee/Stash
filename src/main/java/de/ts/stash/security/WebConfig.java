c2875048aad995d94b756b576c4fa695a0b88b0c
MuesLee <stimo@gmx.net>
2019-02-23 12:12:42 +0100

    Replaced deprecated WebMvcConfigurerAdapter

--- src/main/java/de/ts/stash/security/WebConfig.java
+++ src/main/java/de/ts/stash/security/WebConfig.java
@@ -2,11 +2,10 @@ package de.ts.stash.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SuppressWarnings("deprecation")
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
