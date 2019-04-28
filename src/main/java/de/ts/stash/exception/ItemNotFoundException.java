7b2baa04f650f30b3c5304afa159f1eaccdf77fc
MuesLee <stimo@gmx.net>
2019-04-28 13:01:28 +0200

    Response Status Exception

--- src/main/java/de/ts/stash/exception/ItemNotFoundException.java
+++ src/main/java/de/ts/stash/exception/ItemNotFoundException.java
@@ -1,5 +1,9 @@
 package de.ts.stash.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code=HttpStatus.NOT_FOUND, reason="Item not found.")
 public class ItemNotFoundException extends RuntimeException {
 
 	/**
